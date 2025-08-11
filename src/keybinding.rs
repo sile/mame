cf69c6df1fb85bb9c2226beca446f47852e52687
Takeru Ohta <phjgt308@gmail.com>
2025-08-11 19:47:15 +0900

    refactor: improve KeymapManager API with private fields and validation

--- src/keybinding.rs
+++ src/keybinding.rs
@@ -2,46 +2,50 @@ use std::collections::BTreeMap;
 
 use crate::KeyMatcher;
 
-pub type ContextName = String;
 pub type Action = (); // TODO:
 
 #[derive(Debug, Clone)]
 pub struct KeymapRegistry {
-    pub contexts: BTreeMap<ContextName, Keymap>,
+    pub contexts: BTreeMap<String, Keymap>, // TODO: private
 }
 
 #[derive(Debug, Clone)]
 pub struct Keymap {
-    pub bindings: BTreeMap<KeyMatcher, Action>,
+    pub bindings: BTreeMap<KeyMatcher, Action>, // TODO: private
 }
 
 #[derive(Debug, Clone)]
 pub struct KeymapManager {
-    pub registry: KeymapRegistry,
-    pub current_context: ContextName,
+    registry: KeymapRegistry,
+    context: String,
 }
 
 impl KeymapManager {
-    pub fn new(registry: KeymapRegistry, initial_context: ContextName) -> Self {
-        Self {
+    pub fn new(registry: KeymapRegistry, initial_context: &str) -> Option<Self> {
+        Some(Self {
             registry,
-            current_context: initial_context,
+            context: initial_context.to_owned(),
+        })
+    }
+
+    pub fn set_context(&mut self, context: &str) -> bool {
+        if self.registry.contexts.contains_key(context) {
+            self.context = context.to_owned();
+            true
+        } else {
+            false
         }
     }
 
-    pub fn set_context(&mut self, context: ContextName) {
-        self.current_context = context;
+    pub fn context(&self) -> &str {
+        &self.context
     }
 
-    pub fn get_current_context(&self) -> &ContextName {
-        &self.current_context
-    }
-
-    pub fn get_registry(&self) -> &KeymapRegistry {
+    pub fn registry(&self) -> &KeymapRegistry {
         &self.registry
     }
 
-    pub fn get_registry_mut(&mut self) -> &mut KeymapRegistry {
-        &mut self.registry
+    pub fn keymap(&self) -> &Keymap {
+        self.registry.contexts.get(&self.context).expect("bug")
     }
 }
