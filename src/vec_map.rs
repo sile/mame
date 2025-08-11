6078fad49a775949fd5965c4416e8b75c7586b89
Takeru Ohta <phjgt308@gmail.com>
2025-08-11 20:18:32 +0900

    Add key-value operations and iterators to VecMap implementation

--- src/vec_map.rs
+++ src/vec_map.rs
@@ -1,12 +1,43 @@
+use std::borrow::Borrow;
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct VecMap<K, V>(Vec<(K, V)>);
 
-impl<K, V> VecMap<K, V>
-where
-    K: PartialEq,
-{
+impl<K: Eq, V> VecMap<K, V> {
     pub fn new() -> Self {
         Self::default()
+    }
+
+    pub fn contains_key<Q: ?Sized>(&self, k: &Q) -> bool
+    where
+        K: Borrow<Q>,
+        Q: Eq,
+    {
+        self.0.iter().find(|x| x.0.borrow() == k).is_some()
+    }
+
+    pub fn get<Q: ?Sized>(&self, k: &Q) -> Option<&V>
+    where
+        K: Borrow<Q>,
+        Q: Eq,
+    {
+        self.0.iter().find(|x| x.0.borrow() == k).map(|x| &x.1)
+    }
+
+    pub fn insert(&mut self, k: K, v: V) {
+        self.0.push((k, v));
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = (&K, &V)> {
+        self.0.iter().map(|(k, v)| (k, v))
+    }
+
+    pub fn keys(&self) -> impl Iterator<Item = &K> {
+        self.0.iter().map(|(k, _)| k)
+    }
+
+    pub fn values(&self) -> impl Iterator<Item = &V> {
+        self.0.iter().map(|(_, v)| v)
     }
 }
 
