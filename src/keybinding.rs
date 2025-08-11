fe49adebcf2079b092a41bbd35c40ccf227b7901
Takeru Ohta <phjgt308@gmail.com>
2025-08-11 14:25:14 +0900

    Refactor keybinding module: rename types and add KeymapManager for context management

--- src/keybinding.rs
+++ src/keybinding.rs
@@ -2,12 +2,46 @@ use std::collections::BTreeMap;
 
 use crate::KeyMatcher;
 
+pub type ContextName = String;
+pub type Action = (); // TODO:
+
 #[derive(Debug, Clone)]
-pub struct KeyBindingsTable {
-    pub contexts: BTreeMap<String, KeyBindings>,
+pub struct KeymapRegistry {
+    pub contexts: BTreeMap<ContextName, Keymap>,
 }
 
 #[derive(Debug, Clone)]
-pub struct KeyBindings {
-    pub bindings: BTreeMap<KeyMatcher, ()>,
+pub struct Keymap {
+    pub bindings: BTreeMap<KeyMatcher, Action>,
+}
+
+#[derive(Debug, Clone)]
+pub struct KeymapManager {
+    pub registry: KeymapRegistry,
+    pub current_context: ContextName,
+}
+
+impl KeymapManager {
+    pub fn new(registry: KeymapRegistry, initial_context: ContextName) -> Self {
+        Self {
+            registry,
+            current_context: initial_context,
+        }
+    }
+
+    pub fn set_context(&mut self, context: ContextName) {
+        self.current_context = context;
+    }
+
+    pub fn get_current_context(&self) -> &ContextName {
+        &self.current_context
+    }
+
+    pub fn get_registry(&self) -> &KeymapRegistry {
+        &self.registry
+    }
+
+    pub fn get_registry_mut(&mut self) -> &mut KeymapRegistry {
+        &mut self.registry
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,6 @@ mod keybinding;
 mod keymatcher;
 mod terminal;
 
+pub use keybinding::{KeymapRegistry, Keymap, KeymapManager};
 pub use keymatcher::KeyMatcher;
 pub use terminal::{UnicodeCharWidthEstimator, UnicodeTerminalFrame, char_cols, str_cols};
