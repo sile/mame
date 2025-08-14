a45fe32a6f366198793dc7b04fafdcf676e326a8
Takeru Ohta <phjgt308@gmail.com>
2025-08-14 18:32:02 +0900

    Rename KeyConfig to Config and add context management methods

---
+++ src/config.rs
@@ -0,0 +1,32 @@
+use crate::{KeyLabels, KeymapRegistry};
+
+#[derive(Debug)]
+pub struct Config<T> {
+    pub keybindings: KeymapRegistry<T>, // TODO: private
+    pub keylabels: KeyLabels,           // TODO: private
+}
+
+impl<T> Config<T> {
+    pub fn parse(
+        value: nojson::RawJsonValue<'_, '_>,
+        main_context: &str,
+    ) -> Result<Self, nojson::JsonParseError> {
+        todo!()
+    }
+
+    pub fn set_context(&mut self, context: &str) -> bool {
+        todo!()
+    }
+
+    pub fn context(&self) -> &str {
+        todo!()
+    }
+
+    pub fn get_entry(&self, key: tuinix::KeyInput) -> Option<&T> {
+        todo!()
+    }
+
+    pub fn entries(&self) -> impl '_ + Iterator<Item = (String, &T)> {
+        std::iter::empty()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
+mod config;
 mod keybinding;
-mod keyconfig;
 mod keymatcher;
 mod terminal;
 mod vec_map;
@@ -7,9 +7,9 @@ mod vec_map;
 // TODO: mod error_dialog or notification_area;
 mod external_command;
 
+pub use config::Config;
 pub use external_command::ExternalCommand;
 pub use keybinding::{Keymap, KeymapManager, KeymapRegistry};
-pub use keyconfig::KeyConfig;
 pub use keymatcher::{KeyLabels, KeyMatcher};
 pub use terminal::{UnicodeCharWidthEstimator, UnicodeTerminalFrame, char_cols, str_cols};
 pub use vec_map::VecMap;
