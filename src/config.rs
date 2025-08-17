47b6e0ba23307044c84c9df53ffb44e73b820308
Takeru Ohta <phjgt308@gmail.com>
2025-08-17 22:22:03 +0900

    Add context field to Config struct and update example configuration

--- src/config.rs
+++ src/config.rs
@@ -2,13 +2,14 @@ use crate::KeymapRegistry;
 
 #[derive(Debug)]
 pub struct Config<T> {
-    pub keybindings: KeymapRegistry<T>, // TODO: private
+    pub context: String,
+    pub kaymap_registry: KeymapRegistry<T>, // TODO: private
 }
 
 impl<T> Config<T> {
     pub fn parse(
         value: nojson::RawJsonValue<'_, '_>,
-        main_context: &str,
+        default_context: &str,
     ) -> Result<Self, nojson::JsonParseError> {
         todo!()
     }
@@ -23,9 +24,5 @@ impl<T> Config<T> {
 
     pub fn get_entry(&self, key: tuinix::KeyInput) -> Option<&T> {
         todo!()
-    }
-
-    pub fn entries(&self) -> impl '_ + Iterator<Item = (String, &T)> {
-        std::iter::empty()
     }
 }
