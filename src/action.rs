5982b66aa797314204afa797f4bb918c8687e235
Takeru Ohta <phjgt308@gmail.com>
2025-08-18 20:42:08 +0900

    Add default implementation for Action::validate method with unused parameter warning suppression

--- src/action.rs
+++ src/action.rs
@@ -3,9 +3,12 @@ use crate::Config;
 pub trait Action:
     Sized + for<'text, 'raw> TryFrom<nojson::RawJsonValue<'text, 'raw>, Error = nojson::JsonParseError>
 {
+    #[expect(unused_variables)]
     fn validate(
         &self,
         value: nojson::RawJsonValue<'_, '_>,
         config: &Config<Self>,
-    ) -> Result<(), nojson::JsonParseError>;
+    ) -> Result<(), nojson::JsonParseError> {
+        Ok(())
+    }
 }
